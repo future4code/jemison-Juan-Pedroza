5dd200bfa7260dddbeb522f6660d26e21a7f2d8c
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-04-11 23:15:26 -0300

    adicionando indexes

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,74 @@
+/*1.
+10
+10,5 */
+
+///////////////////////////////////////////
+
+//2. 10,10,10
+
+///////////////////////////////////////////
+
+//3.
+/*let cargaHoraria = prompt("Quantas horas você trabalha por dia?")
+let salHora = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${salHora/cargaHoraria} por hora`)*/
+
+///////////////////////////////////////////
+//1.
+
+/*let nome;
+let idade;
+
+console.log(typeof nome, typeof idade);*/
+
+//d. Tipo undefined pois nenhum valor foi atribuido.
+
+//f. Ambos resultados como String pois tudo que sai do Prompt é String.
+
+
+/*const nome = prompt("Nome completo:");
+let idade = prompt("Insira sua idade:");
+
+console.log("Olá",nome,"você tem", idade, "anos.");*/
+
+///////////////////////////////////////////
+
+//2.
+/*let perg1 = "Você está usando camiseta cinza?";
+let perg2 = "Você está usando tênis nesse momento?";
+let perg3 = "Você está usando relógio agora?";
+
+let camiseta = prompt("Você está usando camiseta cinza?");
+let tenis = prompt("Você está usando tênis nesse momento?");
+let relogio = prompt("Você está usando relógio agora?");
+
+console.log(perg1, camiseta);
+console.log(perg2, tenis);
+console.log(perg3, relogio);*/
+
+///////////////////////////////////////////
+
+//3.
+/*let a = 10;
+let b = 25;
+let c;
+
+console.log(a,b);
+
+c = a;
+a = b;
+b = c;
+
+console.log(a,b);*/
+
+///////////////////////////////////////////
+
+//Desafio
+
+let primeiroNum = prompt("Insira o primeiro número.");
+let segundoNum = prompt("Insira o segundo número.");
+let soma = Number(primeiroNum) + Number(segundoNum);
+let mult = primeiroNum * segundoNum;
+
+console.log("O primeiro número:", primeiroNum, "somado ao segundo:", segundoNum, "resulta em:", soma);
+console.log("O primeiro número:", primeiroNum, "multiplicado ao segundo:", segundoNum, "resulta em:", mult);
