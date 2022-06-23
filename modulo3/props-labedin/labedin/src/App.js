2c1a75d989cb5f5d5505b43a00db13cf42b757e5
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-06-23 01:35:30 -0300

    adicionando site com exercicio props

---
+++ modulo3/props-labedin/labedin/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import minhaFoto from './img/Juan.jpg';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem={minhaFoto}
+          nome="Juan Carlos" 
+          descricao="Oi, eu sou o Juan. Sou um desenvolvedor fullStack e modelador 3D."
+        />
+        
+        <ImagemButton 
+          imagem="https://cdn-icons-png.flaticon.com/512/271/271210.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <CardPequeno
+        imagem={minhaFoto}
+        nome="Email:"
+        descricao="juan.torterolo@gmail.com"
+      />
+
+      <CardPequeno
+        imagem={minhaFoto}
+        nome="Endereço:"
+        descricao="R: Dr. Licinio Maragliano, 309"
+      />
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Curso de desenvolvimento Fullstack com foco em Javascript e NodeJS!" 
+        />
+        
+        <CardGrande 
+          imagem="https://logodownload.org/wp-content/uploads/2019/03/nasa-logo-0-768x768.png" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
