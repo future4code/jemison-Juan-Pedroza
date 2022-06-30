8ccc2df08216da3d97efe7f09fc5ebc0b2a453d1
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-06-30 19:31:15 -0300

    Adicionando site e surge com exercicio de styled components - LabZap

--- modulo3/styled-components/src/App.js
+++ modulo3/styled-components/src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Header} from './Components/Header/Header';
 
-import Logo from './img/LabZap.svg'
+import { Header } from './Components/Header/Header';
+import { AsideLeft } from './Components/Nav/Nav';
+import { AsideRight } from './Components/Aside/Aside';
+import { Footer } from './Components/Footer/Footer';
+import { Main } from './Components/Main/Main';
+
+import Logo from "./img/LabZap.svg"
+
+const Body = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 12% 1fr 12%;
+  grid-template-rows: 12% 1fr 5%;
+  background: white;
+`
 
 function App() {
   return (
-    <div className="App">
-        <Header logo={Logo}/>
-      <nav>
-      </nav>
-      <main>
-        <label htmlFor="#">Remetente:</label>
-        <input type="text" />
-        <label htmlFor="#">Msg:</label>
-        <input type="text" />
-        <button>
-          Enviar Mensagem
-        </button>
-      </main>
-      <aside>
-      </aside>
-      <footer>
-      </footer>
-    </div>
+    <Body>
+      <Header logo={Logo} />
+      <AsideLeft />
+      <Main />
+      <AsideRight />
+      <Footer />
+    </Body>
   );
 }
 
--- modulo3/styled-components/src/Components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components"
-
-
-export function Header (props) {
-    return (<div>
-            <img src={props.logo} alt="Logo-Labenu" />
-        </div>)
-}
