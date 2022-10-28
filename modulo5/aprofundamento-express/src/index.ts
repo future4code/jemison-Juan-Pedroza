344ad68bc1395612e5cf45fc244e75e28aa6bfd1
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-10-28 02:06:49 -0300

    aprofundamento express iniciado

---
+++ modulo5/aprofundamento-express/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express"
+import cors from 'cors'
+import {todos} from './data'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+type TODOS = {
+    userId:number,
+    id:number,
+    title:string,
+    completed:boolean
+}
+
+app.get('/ping', (req: Request, res: Response) => {
+    const filtraArray = () => {
+    }
+})
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003");
+});
