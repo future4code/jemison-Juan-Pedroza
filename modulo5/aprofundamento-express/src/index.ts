d0156587c385e60e46a0a331d6d49cb3cb0fa209
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-10-29 01:05:20 -0300

    atualizando exercicios

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express"
 import cors from 'cors'
-import {todos} from './data'
+import { todos } from './data'
 
 const app = express()
 
@@ -8,18 +8,34 @@ app.use(express.json())
 app.use(cors())
 
 type TODOS = {
-    userId:number,
-    id:number,
-    title:string,
-    completed:boolean
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
 }
 
 app.get('/ping', (req: Request, res: Response) => {
-    const filtraArray = () => {
-        for ()
-    }
+    const bla = todos.filter((todo) => {
+        if (todo.completed === false) {
+            return todo
+        }
+    })
+    res.send(bla)
 })
+
+app.post('/posting', (req: Request, res: Response) => {
+    const postInfo:TODOS = req.body
+    const postToAdd = req.headers.auth
+
+    if(!postInfo || !postToAdd){
+        res.status(400).send('deu ruim') 
+    }
+
+    res.status(200).send('resposta do servidor')
+})
+
+
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
-});
+})
